3a4254727d73d1784c3c14ef70c667c29b43b6fc
tanushmusham <tanushmusham@gmail.com>
2025-11-26 13:43:08 -0600

    none

--- src/main/java/SttService.java
+++ src/main/java/SttService.java
@@ -72,9 +72,6 @@ public class SttService implements AutoCloseable {
 
     private LiveSpeechRecognizer buildRecognizer() throws IOException {
         Configuration configuration = new Configuration();
-        configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
-        configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
-        configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
         return new LiveSpeechRecognizer(configuration);
     }
 
